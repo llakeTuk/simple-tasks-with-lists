72d9407a1712a9a875ec0112a86e88d3cd70dcec
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-26 22:17:59 +0300

    Update list_3.py
    Returns number of each letter in entered string

--- list_3.py
+++ list_3.py
@@ -1,12 +1,14 @@
-origin_string = input('enter string: ')
-list_string = [origin_string[0]]
-list_count = [0]
-for i in origin_string:
-  if list_string.count(i) == 0:
-    list_string.append(i)
-    list_count.append(1)
-  else:
-    j = list_string.index(i)
-    list_count[j] += 1
-print(*list_string)
-print(*list_count) "update to print in one row(a: 1, b: 2)"
+y = 'y'
+While y == 'y':  
+  origin_string = input('enter string: ')
+  list_string = [origin_string[0]]
+  list_count = [0]
+  for i in origin_string:
+    if list_string.count(i) == 0:
+      list_string.append(i)
+      list_count.append(1)
+    else:
+      j = list_string.index(i)
+      list_count[j] += 1
+  print(*list_string)
+  print(*list_count) "update to print in one row(a: 1, b: 2)"
