20e6c98d24be69975af5ab561efef9ab8d0ebcb8
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-27 14:25:50 +0300

    Update list_2.py

--- list_2.py
+++ list_2.py
@@ -1,5 +1,13 @@
-a = [2, 4, 5, 1, 6, 3, 12, 56, 34, 78, 65, 99, 121, 13]
-b = [1, 2, 5, 44, 33, 22, 12, 34, 55, 64, 121, 144, 35]
+y = 'y'
+while y == 'y':
+  input_data_alist = input("Enter A-list of numbers, using comma: ")
+  Input_data_blist = input("Enter B-list of numbers, using comma: ")
+  splited_input_data = input_data.split(',')
+  Splited_input
+  int_input_data = map(int, splited_input_data)
+  alist = list(int_input_data)
+  a = [2, 4, 5, 1, 6, 3, 12, 56, 34, 78, 65, 99, 121, 13]
+  b = [1, 2, 5, 44, 33, 22, 12, 34, 55, 64, 121, 144, 35]
 c = []
 for i in a:
   if b.count(i) != 0:
