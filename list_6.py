9bb49d38c7c694a44e3b8b5e06c7055d90203118
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-07-02 16:17:36 +0300

    Update list_6.py

--- list_6.py
+++ list_6.py
@@ -1,8 +1,8 @@
 def useless_list(input_list):
-  return max(input_list) / len(input_list)
+  return round(max(input_list) / len(input_list, 4)
 y = 'y'
 while y == 'y':
-  input_list = input('enter list of elements, using comma: ').split(',')
+  input_list = input('enter list of float elements, using comma: ').split(',')
   float_input_list = list(map(float, input_list))
   print(useless_list(float_input_list))
   y = input('restart?(y/n): ')
