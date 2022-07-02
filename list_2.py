2cc362b4b9cbd935c3f0e52cdeab92a2d840db84
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-07-02 15:52:42 +0300

    Update list_2.py

--- list_2.py
+++ list_2.py
@@ -1,11 +1,9 @@
 y = 'y'
 while y == 'y':
-  input_data_alist = input("Enter A-list of numbers, using comma: ")
-  input_data_blist = input("Enter B-list of numbers, using comma: ")
-  splited_input_data_a = input_data_alist.split(',')
-  splited_input_data_b = input_data_blist.split(',')
-  int_input_data_a = map(int, splited_input_data_a)
-  int_input_data_b = map(int, splited_input_data_b)
+  input_data_alist = input("Enter A-list of numbers, using comma: ").split(',')
+  input_data_blist = input("Enter B-list of numbers, using comma: ").split(',')
+  int_input_data_a = map(int, input_data_alist)
+  int_input_data_b = map(int, input_data_blist)
   alist = list(int_input_data_a)
   blist = list(int_input_data_b)
   clist = []
