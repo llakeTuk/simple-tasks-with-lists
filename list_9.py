b08e0a083f7ba6f9fc15464d3d8df6dc2d62cb5c
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-08-14 21:43:55 +0300

    Update list_9.py

--- list_9.py
+++ list_9.py
@@ -2,6 +2,7 @@ def molecula_count(atom_list, molecula_list):
   for i in range(len(atom_list)):
     atom_list[i] = atom_list[i] // molecula_list[i]
   return print('number of possible molecules: ', min(atom_list))
+def formula_analysis:
 y = 'y'
 while y == 'y':
   input_atoms = input('enter number of atoms C, O, H, using comma: ').split(',')
